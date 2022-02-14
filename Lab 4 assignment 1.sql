a0ffcd6c12fb380d5852b99c4ddd1a966d16f0fe
GG-333 <95605947+GG-333@users.noreply.github.com>
2022-02-15 00:53:35 +0530

    Add files via upload

---
+++ Lab 4 assignment 1.sql
@@ -0,0 +1,41 @@
+insert into supplier values(1, 'Rajesh Retails', 'Delhi', '1234567890');
+insert into supplier values(2, 'Appario Ltd.', 'Mumbai', '2589631470');
+insert into supplier values(3, 'Knome products', 'Banglore', '9785462315');
+insert into supplier values(4, 'Bansal Retails', 'Kochi', '8975463285');
+insert into supplier values(5, 'Mittal Ltd.', 'Lucknow', '7898456532');
+
+insert into customer values(1, 'AAKASH', '9999999999', 'DELHI', 'M');
+insert into customer values(2, 'AMAN', '9785463215', 'NOIDA', 'M');
+insert into customer values(3, 'NEHA', '9999999999', 'MUMBAI', 'F');
+insert into customer values(4, 'MEGHA', '9994562399', 'KOLKATA', 'F');
+insert into customer values(5, 'PULKIT', '7895999999', 'LUCKNOW', 'M');
+
+insert into `category` values(1, 'BOOKS');
+insert into `category` values(2, 'GAMES');
+insert into `category` values(3, 'GROCERIES');
+insert into `category` values(4, 'ELECTRONICS');
+insert into `category` values(5, 'CLOTHES');
+
+insert into `product` values(1, 'GTA V', 'DFJDJFDJFDJFDJFJF', '2');
+insert into `product` values(2, 'TSHIRT', 'DFDFJDFJDKFD', '5');
+insert into `product` values(3, 'ROG LAPTOP', 'DFNTTNTNTERND', '4');
+insert into `product` values(4, 'OATS', 'REURENTBTOTH', '3');
+insert into `product` values(5, 'HARRY POTTER', 'NBEMCTHTJTH', '1');
+
+insert into product_details values(1,1,2,1500);
+insert into product_details values(2,3,5,30000);
+insert into product_details values(3,5,1,3000);
+insert into product_details values(4,2,3,2500);
+insert into product_details values(5,4,1,1000);
+
+insert into `order` values(20,1500,"2021-10-12",3,5);
+insert into `order` values(25,30500,"2021-09-16",5,2);
+insert into `order` values(26,2000,"2021-10-05",1,1);
+insert into `order` values(30,3500,"2021-08-16",4,3);
+insert into `order` values(50,2000,"2021-10-06",2,1);
+
+insert into `rating` values(1,2,2,4);
+insert into `rating` values(2,3,4,3);
+insert into `rating` values(3,5,1,5);
+insert into `rating` values(4,1,3,2);
+insert into `rating` values(5,4,5,4);
